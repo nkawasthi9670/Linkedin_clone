0c241b2193233cf778e745556f468b4ebfeb28d7
Harshit Sharma <sharmaharshit0808@gmail.com>
2024-12-08 12:05:15 +0530

    4- Post Routes created

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -1,20 +1,42 @@
-import { mailtrapClient,sender } from "../lib/mailtrap.js";
-import { createWelcomeEmailTemplate } from "./emailTemplates.js";
+import { mailtrapClient, sender } from "../lib/mailtrap.js";
+import { createCommentNotificationEmailTemplate, createWelcomeEmailTemplate } from "./emailTemplates.js";
 
-export const sendWelcomeEmail=async(email,name,profileUrl)=>{
-    const recipient=[{email}]
+export const sendWelcomeEmail = async (email, name, profileUrl) => {
+    const recipient = [{ email }]
 
     try {
         const response = await mailtrapClient.send({
-            from:sender,
-            to:recipient,
-            subject:"Welcome to LinkedIn Clone",
-            html:createWelcomeEmailTemplate(name,profileUrl),
-            category:"welcome"
+            from: sender,
+            to: recipient,
+            subject: "Welcome to LinkedIn Clone",
+            html: createWelcomeEmailTemplate(name, profileUrl),
+            category: "welcome"
         })
 
-        console.log("Welcome email sent successfully",response);
+        console.log("Welcome email sent successfully", response);
     } catch (error) {
         throw error;
     }
+}
+
+export const sendCommentNotificationEmail = async (recipientEmail,
+    recipientName,
+    commenterName,
+    postUrl,
+    commentContent) => {
+        const recipient=[{email:recipientEmail}];
+        try {
+            const response=await mailtrapClient.send({
+                from:sender,
+                to:recipient,
+                subject:"New comment on your post",
+                html:createCommentNotificationEmailTemplate(recipientName,commenterName,
+                    postUrl,commentContent
+                ),
+                category:"comment_notification"
+            })
+            console.log("Comment notification Email sent successfully",response );
+        } catch (error) {
+            throw error;
+        }
 }
