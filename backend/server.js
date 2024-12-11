c4b9d59203ef001f9998a3024ef0969232911258
Harshit Sharma <sharmaharshit0808@gmail.com>
2024-12-11 13:55:44 +0530

    7- Frontend setup and SignUpPage created

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,18 @@ import notificationRoutes from "./routes/notification.route.js"
 import connectionRoutes from "./routes/connection.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
+app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
 
 app.use("/api/v1/auth",authRoutes);
