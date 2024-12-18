a40d12eb772a430e299d2bd1fc26cb7c6299ac13
Harshit Sharma <sharmaharshit0808@gmail.com>
2024-12-18 20:15:39 +0530

    Project completed with deployement updates

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,21 @@ import connectionRoutes from "./routes/connection.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+const __dirname=path.resolve();
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173",
+        credentials:true,
+    }));
+}
+
 app.use(express.json({limit:"5mb"}));
 app.use(cookieParser());
 
@@ -26,6 +31,14 @@ app.use("/api/v1/users",userRoutes);
 app.use("/api/v1/posts",postRoutes);
 app.use("/api/v1/notifications",notificationRoutes);
 app.use("/api/v1/connections",connectionRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
