d825d4cc626a23abea2c8d5a8c6e146a48de11ad
Nikhil Kumar Awasthi <nikhilkumarawasthi11@gmail.com>
2025-08-04 23:13:21 +0530

    update axios baseURL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE==="development"?"http://localhost:5000/api/v1":"/api/v1",
-    withCredentials: true,
-})
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/v1"
+    : "https://linkedin-clone-orqp.onrender.com/api/v1", 
+  withCredentials: true,
+});
