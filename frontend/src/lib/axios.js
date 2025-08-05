39d2587aadd5549dc5be9489d0bfac28520e600f
Nikhil Kumar Awasthi <nikhilkumarawasthi11@gmail.com>
2025-08-05 16:43:25 +0530

    Fix: Added base path for Vercel deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:5000/api/v1"
-    : "https://linkedin-clone-orqp.onrender.com/api/v1", 
+    :  "https://linkedin-clone-orqp.onrender.com/api/v1", 
   withCredentials: true,
 });
